f25cc5b9d8a95c6c792a7b2644a9c7db002ce47c
dresdner353 <dresdner353@gmail.com>
2017-04-13 21:47:53 +0100

    Reworked on/off timing to use an integer derived from hhmm giving us a number 0000 to 2359. Made the script work a lot better

--- jbhasd_server.py
+++ jbhasd_server.py
@@ -78,18 +78,23 @@ zeroconf_delay_secs = 60
 last_sunset_check = -1
 sunset_epoch = 0
 
-# Lights on 1 hour after sunset
+# Lights on N seconds after sunset
 sunset_lights_on_offset = 1800
 
-# Lights off at given AM hour
-lights_off_hour = 2
+# Lights off at given HHMM time
+# Time here is a 4 digit decimal number
+# Good for numerical comparisons
+lights_off_time = int("0200")
 
 while (1):
 
     # Sunset check
     now = time.time()
-    current_hour = datetime.datetime.today().hour
-    if (now - last_sunset_check >= 24*60*60):
+    current_time = int(time.strftime("%H%M", time.localtime()))
+
+    # recalc every 12 hours or at midnight
+    if ((now - last_sunset_check) >= 12*60*60 or
+        (current_time == 0)):
         # Re-calculate
         print("Getting Sunset times..")
         response = None
@@ -104,8 +109,8 @@ while (1):
             json_data = json.loads(response_str.decode('utf-8'))
             sunset_str = json_data['results']['sunset']
             sunset_ts = sunset_api_time_to_epoch(sunset_str)
-            print("Sunset: %s (%d)" % (time.ctime(sunset_ts),
-                                       sunset_ts))
+            lights_on_time = int(time.strftime("%H%M", time.localtime(sunset_ts + sunset_lights_on_offset)))
+            print("Lights on at: (%04d)" % (lights_on_time))
 
         last_sunset_check = now
 
@@ -120,11 +125,26 @@ while (1):
     time.sleep(zeroconf_delay_secs)
     zeroconf.close()
 
-    print("Lights on at: %s (%d)" % (time.ctime(sunset_ts + sunset_lights_on_offset),
-                                     sunset_ts + sunset_lights_on_offset))
-    print("Lights off at hour: %d" % (lights_off_hour))
-    
-    print ("Service List:")
+    # Calculate desired state
+    desired_state = 0
+    if (lights_on_time <= lights_off_time):
+        if (current_time >= lights_on_time and 
+            current_time < lights_off_time):
+            desired_state = 1
+    else:
+        if (current_time > lights_on_time):
+            desired_state = 1
+        else:
+            if (current_time < lights_on_time and
+                current_time < lights_off_time):
+                desired_state = 1
+
+    print("Current Time: (%04d)" % (current_time))
+    print("Lights on at: (%04d)" % (lights_on_time))
+    print("Lights off at hour: %04d" % (lights_off_time))
+    print("Desired State for Lights: %d" % (desired_state))
+
+    print ("Discovered Devices:")
     for key in jbhasd_url_dict.keys():
         print("\nName:%s URL:%s" % (key, jbhasd_url_dict[key]))
 
@@ -153,22 +173,13 @@ while (1):
                 if (check_switch(zone_name, control_name)):
                     print("  Marked for Sunrise/Sunset automation")
 
-                    # Determine desired state
-                    # If the hour is the lights off hour then 
-                    # its off
-                    # else if the curent time has passed sunset plus
-                    # offset then we're on
-                    desired_state = 0
-                    if (current_hour == lights_off_hour):
-                        desired_state = 0
-                        print("  Reached lights off hour")
-                    else:
-                        if (now >= (sunset_ts + sunset_lights_on_offset)):
-                            print("  Time is past sunset offset")
-                            desired_state = 1
+                    # current_time
+                    # lights_on_time
+                    # lights_off_time
 
+                   
                     if (desired_state != control_state):
-                        print("==========> Changing state from %d to %d" % (control_state, desired_state))
+                        print("  ==========> Changing state from %d to %d" % (control_state, desired_state))
                         data = urllib.parse.urlencode({'control' : control_name, 'state'  : desired_state})
                         post_data = data.encode('utf-8')
                         req = urllib.request.Request(jbhasd_url_dict[key], post_data)
