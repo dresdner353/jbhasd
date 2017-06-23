6e6bb8320af6064a40bd4606bec2b95e605939d6
dresdner353 <dresdner353@gmail.com>
2017-06-23 12:06:54 +0100

    tidied up code to list JSON status details from all devices

--- zero_discover.py
+++ zero_discover.py
@@ -7,16 +7,38 @@ from six.moves import input
 from zeroconf import ServiceBrowser, Zeroconf
 import socket
 import time
+import urllib
+import urllib.parse
+import urllib.request
+import urllib.error
+
+url_set = set()
+http_timeout_secs = 2
 
 class MyListener(object):  
     def remove_service(self, zeroconf, type, name):
         print("\nService %s removed" % (name))
+        return
 
     def add_service(self, zeroconf, type, name):
         info = zeroconf.get_service_info(type, name)
         address = socket.inet_ntoa(info.address)
         port = info.port
-        print("\nDiscovered: %s\nURL: http://%s:%d\nInfo:%s\n" % (name, address, port, info))
+        url = 'http://%s:%d' % (address, port)
+        url_set.add(url)
+        #print("\nDiscovered: %s\nURL: %s\nInfo:%s\n" % (name, url, info))
+        json_url = '%s/json' % (url)
+
+        response = None
+        try:
+            response = urllib.request.urlopen(json_url, 
+                                              timeout = http_timeout_secs)
+        except:
+            print("Error in urlopen (status check)")
+
+        if response is not None:
+            response_str = response.read()
+            print("%s\n%s\n" % (json_url, response_str))
 
 zeroconf = Zeroconf()  
 listener = MyListener()  
