d7c0f0d0700710b38070a5db001610a170f986cc
Cormac Long <dresdner353@gmail.com>
2017-07-20 10:13:02 +0100

    Added simulation and temp/humidity sensors

--- jbhasd_device_sim.py
+++ jbhasd_device_sim.py
@@ -20,6 +20,8 @@ us_states_list = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Col
 
 irish_counties_list = ['Antrim', 'Armagh', 'Carlow', 'Cavan', 'Clare', 'Cork', 'Derry', 'Donegal', 'Down', 'Dublin', 'Fermanagh', 'Galway', 'Kerry', 'Kildare', 'Kilkenny', 'Laois', 'Leitrim', 'Limerick', 'Longford', 'Louth', 'Mayo', 'Meath', 'Monaghan', 'Offaly', 'Roscommon', 'Sligo', 'Tipperary', 'Tyrone', 'Waterford', 'Westmeath', 'Wexford', 'Wicklow']
 
+irish_rivers_list = ['Shannon', 'Barrow', 'Suir', 'Blackwater', 'Bann', 'Nore', 'Suck', 'Liffey', 'Erne', 'Foyle', 'Slaney', 'Boyne', 'Moy', 'Clare', 'Blackwater', 'Inny', 'Lee', 'Lagan', 'Brosna', 'Laune', 'Feale', 'Bandon', 'Blackwater', 'Annalee', 'Bride', 'Boyle ', 'Deel', 'Robe', 'Finn', 'Maigue', 'Fane ', 'Ballisodare', 'Dee', 'Fergus', 'Little Brosna', 'Mulkear ', 'Glyde']
+
 # dictionary of simulated json status dicts
 # indexed on port value
 json_status_dict = {}
@@ -60,21 +62,22 @@ class device_web_server(object):
     # Force trailling slash off on called URL
     index._cp_config = {'tools.trailing_slash.on': False}
 
-def change_controls():
+def change_device_status():
     # Randomise changes in the devices
 
     while(1):
         ports_list = list(json_status_dict)
         num_ports = len(ports_list)
 
-        num_devices_to_change = (random.randint(1, 10000000) % num_ports) + 1
+        # controls
+        num_devices_to_change = random.randint(0, 10)
         print("Changing controls for %d devices" % (num_devices_to_change))
         for i in range(0, num_devices_to_change):
             port_index = random.randint(1, 10000000) % num_ports
             port = ports_list[port_index]
             device_json = json_status_dict[port]
             num_controls_in_device = len(device_json['controls'])
-            num_controls_to_change = (random.randint(1, 10000000) % num_controls_in_device) + 1
+            num_controls_to_change = random.randint(1, 10000000) % (num_controls_in_device + 1)
             print("Changing %d controls for %s" % (num_controls_to_change, 
                                                    device_json['name']))
             for j in range(0, num_controls_to_change):
@@ -84,7 +87,29 @@ def change_controls():
                                                            control_state))
                 device_json['controls'][control_index]['state'] = control_state
 
-        time.sleep(5)
+        # sensors
+        num_devices_to_change = random.randint(0, 10)
+        print("Changing sensors for %d devices" % (num_devices_to_change))
+        for i in range(0, num_devices_to_change):
+            port_index = random.randint(1, 10000000) % num_ports
+            port = ports_list[port_index]
+            device_json = json_status_dict[port]
+            num_sensors_in_device = len(device_json['sensors'])
+            num_sensors_to_change = random.randint(1, 10000000) % (num_sensors_in_device + 1)
+            print("Changing %d sensors for %s" % (num_sensors_to_change, 
+                                                   device_json['name']))
+            for j in range(0, num_sensors_to_change):
+                sensor_index = random.randint(1, 10000000) % num_sensors_in_device
+                # Generate randome temp and humidity
+                sensor_temp = "%.2f" % (random.uniform(-30, 95))
+                sensor_humidity = "%.2f" % (random.uniform(0, 100))
+                print("Changing sensor %s temp:%s humidity:%s" % (device_json['sensors'][sensor_index]['name'], 
+                                                                  sensor_temp,
+                                                                  sensor_humidity))
+                device_json['sensors'][sensor_index]['temp'] = sensor_temp
+                device_json['sensors'][sensor_index]['humidity'] = sensor_humidity
+
+        time.sleep(10)
 
 
 # main
@@ -96,9 +121,14 @@ my_ip = get_ip()
 # want to have multiple separate ports, one per device
 cherrypy.tree.mount(device_web_server(), '/json')
 cherrypy.server.unsubscribe()
+# Logging off
+cherrypy.config.update({'log.screen': False,
+                        'log.access_file': '',
+                        'log.error_file': ''})
 server_list = []
 num_states = len(us_states_list)
 num_counties = len(irish_counties_list)
+num_rivers = len(irish_rivers_list)
 
 for id in range(0, num_states):
     # DNS-SD/MDNS
@@ -117,13 +147,22 @@ for id in range(0, num_states):
     # PIck a number and then random index
     # We then module cycle through that index 
     # naming controls after the list entry
-    num_controls = random.randint(1, 5) 
+    num_controls = random.randint(0, 5) 
     control_index = random.randint(0, 1000000) % num_counties
     for i in range(0, num_controls):
         control_state = random.randint(0, 100) % 2
         control_name = irish_counties_list[control_index]
         json_data['controls'].append({'name': control_name, 'type' : 'switch', 'state' : control_state})
         control_index = (control_index + 1) % num_counties
+
+    num_sensors = random.randint(0, 5) 
+    sensor_index = random.randint(0, 1000000) % num_rivers
+    for i in range(0, num_sensors):
+        sensor_temp = "%.2f" % (random.uniform(-30, 95))
+        sensor_humidity = "%.2f" % (random.uniform(0, 100))
+        sensor_name = irish_rivers_list[sensor_index]
+        json_data['sensors'].append({'name': sensor_name, 'type' : 'temp/humidity', 'temp' : sensor_temp, 'humidity' : sensor_humidity})
+        sensor_index = (sensor_index + 1) % num_rivers
 
     json_status_dict[port] = json_data
 
@@ -161,7 +200,7 @@ for id in range(0, num_states):
     
     zeroconf.register_service(info)
 
-random_t = threading.Thread(target = change_controls)
+random_t = threading.Thread(target = change_device_status)
 random_t.daemon = True
 random_t.start()
 
