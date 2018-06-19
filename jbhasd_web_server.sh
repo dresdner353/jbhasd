2acb12868ea7130ff2b2d753a90446f4cb5d171a
dresdner353 <dresdner353@gmail.com>
2018-06-19 14:59:42 +0100

    Added shell script to start web server in background
    Could be run every minute from crontab

---
+++ jbhasd_web_server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# determine path of this script
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+JBHASD_WEB_SVR="jbhasd_web_server.py"
+JBHASD_WEB_SVR_PATH="${SCRIPTPATH}/${JBHASD_WEB_SVR}"
+
+# Check for jbhasd_web_server.py running
+pgrep -f "${JBHASD_WEB_SVR}" >/dev/null
+if [ $? -ne 0 ]
+then
+    nohup ${JBHASD_WEB_SVR_PATH} > /dev/null 2>&1 &
+    exit 0
+fi
+
+exit -1 
