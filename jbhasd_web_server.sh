181a98ea9da1ae4e4acda472b746034aadeff4e4
dresdner353 <dresdner353@gmail.com>
2019-09-08 21:35:21 +0100

    Added logging support to ~/jbhasd_logs where logs are kept up to 20 days after being last written.
    Will help root out various crashes that seem to bother this web server
    by capturing all stdout and err to log files.

--- jbhasd_web_server.sh
+++ jbhasd_web_server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export TZ=Europe/Dublin
+
 # determine path of this script
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
@@ -7,14 +9,22 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 JBHASD_WEB_SVR="jbhasd_web_server.py"
 JBHASD_WEB_SVR_PATH="${SCRIPTPATH}/${JBHASD_WEB_SVR}"
 
-export TZ=Europe/Dublin
+# Logging
+LOG_PURGE_DAYS=20
+HOME_DIR=~
+LOG_DIR="${HOME_DIR}/jbhasd_logs"
+mkdir -p "${LOG_DIR}"
+LOG_PREFIX="jbhasd_web_server"
+LOG_FILE="${LOG_DIR}/${LOG_PREFIX}_`date +%Y%m%d%H%M%S`.log"
 
 # Check for jbhasd_web_server.py running
 pgrep -f "${JBHASD_WEB_SVR}" >/dev/null
 if [ $? -ne 0 ]
 then
-    nohup ${JBHASD_WEB_SVR_PATH} > /dev/null 2>&1 &
-    exit 0
+    nohup ${JBHASD_WEB_SVR_PATH} > ${LOG_FILE} 2>&1 &
 fi
 
-exit -1 
+# Purge old logs
+find ${LOG_DIR} -name ${LOG_PREFIX}* -mtime +${LOG_PURGE_DAYS} -exec rm \{} \; >/dev/null 2>&1
+
+exit 0
